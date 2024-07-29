f56ed9089fd81d9962a5ce55dfb070757930968b
AshenSachinthana <dasachinthana@gmail.com>
2024-07-29 16:11:57 +0530

    complaint details navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -31,7 +31,9 @@ const RootLayout = () => {
     <Stack screenOptions={{headerShown : false}}>
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />      
+      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+      <Stack.Screen name="complaintdetails/[complaintid]" options={{ headerShown: false }} 
+/>       
     </Stack>
   );
 };
