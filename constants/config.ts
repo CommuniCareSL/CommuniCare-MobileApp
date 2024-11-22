9db8c66db20ad0bec8fb3053d82216edbe66ed6f
AshenSachinthana <dasachinthana@gmail.com>
2024-11-22 23:05:05 +0530

    Base URL

---
+++ constants/config.ts
@@ -0,0 +1,7 @@
+// Contains backend API base URL
+
+// Define the base URL for your Spring Boot backend API
+const BASE_URL = 'http://192.168.8.116:8080/api'; 
+
+// Export the base URL
+export default BASE_URL;
