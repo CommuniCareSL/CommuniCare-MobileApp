a882dba47d0cc1a5609eb9cd43285eeb7df6cdee
AshenSachinthana <dasachinthana@gmail.com>
2024-11-28 21:06:11 +0530

    complint categories

---
+++ data/complaintCategories.js
@@ -0,0 +1,64 @@
+const categories = [
+  {
+    id: 1,
+    icon: require('../assets/images/road.png'),
+    title: 'Road hazards',
+  },
+  {
+    id: 2,
+    icon: require('../assets/images/forest.png'),
+    title: 'Unsafe trees in roadside',
+  },
+  {
+    id: 3,
+    icon: require('../assets/images/streetlight.png'),
+    title: 'Street lamp malfunction',
+  },
+  {
+    id: 4,
+    icon: require('../assets/images/drain.png'),
+    title: 'Damages to the street drains',
+  },
+  {
+    id: 5,
+    icon: require('../assets/images/garbage.png'),
+    title: 'Garbage disposal on roadside',
+  },
+  {
+    id: 6,
+    icon: require('../assets/images/mosquito.png'),
+    title: 'Mosquito breeding grounds',
+  },
+  {
+    id: 7,
+    icon: require('../assets/images/toilet.png'),
+    title: 'Issues related to public toilets',
+  },
+  {
+    id: 8,
+    icon: require('../assets/images/animal.png'),
+    title: 'Stray animals',
+  },
+  {
+    id: 9,
+    icon: require('../assets/images/worker.png'),
+    title: 'Unauthorized constructions',
+  },
+  {
+    id: 10,
+    icon: require('../assets/images/seller.png'),
+    title: 'Unauthorized street sellers',
+  },
+  {
+    id: 11,
+    icon: require('../assets/images/wall.png'),
+    title: 'Dangerous Walls or buildings',
+  },
+  {
+    id: 12,
+    icon: require('../assets/images/plus.png'),
+    title: 'Others',
+  },
+];
+
+export default categories;
